2185aeb8a44c96f1ce74123980ea11bb7a726362
QuocNguyen332003 <123507130+QuocNguyen332003@users.noreply.github.com>
2024-05-04 15:12:02 +0700

    Parameter Tampering

--- src/main/java/Filter/SecurityHeaderFilter.java
+++ src/main/java/Filter/SecurityHeaderFilter.java
@@ -22,6 +22,9 @@ public class SecurityHeaderFilter implements Filter {
         // Thêm header X-Content-Type-Options vào mọi HTTP response
         httpResp.setHeader("X-Frame-Options", "SAMEORIGIN");
         httpResp.setHeader("X-Content-Type-Options", "nosniff");
+        httpResp.setHeader("Access-Control-Allow-Origin", "https://fonts.googleapis.com https://fonts.gstatic.com https://stackpath.bootstrapcdn.com https://use.fontawesome.com https://cdnjs.cloudflare.com https://localhost:8443");
+        httpResp.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+        httpResp.setHeader("Content-Security-Policy", "default-src *; script-src 'self'; style-src *; font-src 'self'; connect-src 'self'; * data; frame-src 'none'; frame-ancestors 'none'; media-src 'none'; object-src 'none'; manifest-src 'none'; worker-src 'none'; form-action 'self'");
         chain.doFilter(request, response);
     }
 
